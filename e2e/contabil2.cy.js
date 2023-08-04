dfc59243cd6ece91ef0317bebc87d8323f0f2e93
julian-patrick <julian.garcia@github.com>
2023-08-04 13:11:45 -0400

    Update contabil2.cy.js
    teste

--- e2e/contabil2.cy.js
+++ e2e/contabil2.cy.js
@@ -24,7 +24,7 @@ context('Realizar o login no Contabil', () =>{
 
  
 
-beforeEach(() => {
+beforeEach(() => {     
 
 cy.visit('http://atual.sisprevweb.com.br/contabilweb02/login.aspx')
 cy.clearAllCookies()
