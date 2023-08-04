dfbc4f9ad776a949edee40028fe3499c3516436e
julian-patrick <julian.garcia@github.com>
2023-08-04 13:03:41 -0400

    teste

---
+++ e2e/CAC-TAT.cy.js
@@ -0,0 +1,9 @@
+
+describe(' Central de Atendimento ao Cliente TAT', function() {
+    it('Verificar o titulo da aplicaçao', function(){
+
+        cy.visit('https://google.com')
+        //cy.visit('./src/index.html')
+
+    })
+})
